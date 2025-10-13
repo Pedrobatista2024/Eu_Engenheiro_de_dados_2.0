2b28659677c59a1318d3aa1ffa94071e0f3dac7b
Pedro Batista Neto <pedroplpellegrinni1918@gmail.com>
2025-10-13 14:24:22 -0300

    atividade02 concluida

--- basico/atividade02.py
+++ basico/atividade02.py
@@ -24,6 +24,8 @@
 
 import pandas as pd
 from datetime import datetime
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
 
 data_hoje = datetime.now().strftime('%y%m%d')
 nome_arquivo = f'noticias_brutas_{data_hoje}.json'
@@ -41,6 +43,31 @@ df_fontes_unicas = df_artigos_brutos['nome_fonte'].drop_duplicates().to_frame()
 df_fontes_unicas.reset_index(inplace=True)
 df_fontes_unicas.rename(columns={'index':'id_fonte', 'nome_fonte': 'nome_fonte'}, inplace=True)
 
-df_fontes_unicas['id_fonte'] = df_fontes_unicas['id_fonte']
+df_fontes_unicas['id_fonte'] = df_fontes_unicas['id_fonte']+1
 
 dim_fonte = df_fontes_unicas[['id_fonte', 'nome_fonte']]
+
+mapeamento_fonte_id = dim_fonte.set_index('nome_fonte')['id_fonte'].to_dict()
+
+df_artigos_brutos['id_fonte_fk'] = df_artigos_brutos['nome_fonte'].map(mapeamento_fonte_id)
+
+df_artigos_brutos.reset_index(inplace=True)
+df_artigos_brutos.rename(columns={'index': 'id_artigo'}, inplace=True)
+
+ft_artigos = df_artigos_brutos[['id_artigo', 'id_fonte_fk', 'published_at', 'title', 'url']]
+
+ft_artigos.rename(columns={
+    'published_at': 'data_publicacao',
+    'title': 'titulo'
+}, inplace=True)
+
+
+print("\n--- Resultado Final: dim_fonte.head() ---")
+print(dim_fonte.head())
+print(dim_fonte.columns.to_list())
+
+print('*'*40)
+
+print("\n--- Resultado Final: ft_artigos.head() ---")
+print(ft_artigos.head())
+print(ft_artigos.columns.to_list())
